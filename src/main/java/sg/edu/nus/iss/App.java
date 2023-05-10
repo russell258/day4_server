ccc6e998d145de45f30ffc47b2d15111cfed9247
russell258 <duckie258@gmail.com>
2023-05-10 14:47:05 +0800

    cookie class

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,95 +1,54 @@
 package sg.edu.nus.iss;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.DataInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws NumberFormatException, IOException
     {
-        //2 arguments, 1 for file, 1 for the port the server will start on
-        //1 Argument for file
-        //1 argument for the port for which he server willl start on;
+        String portNumber="";
+        String fileName = "";
+            if (args.length>0){
+                portNumber = args[0];
+                fileName = args[1];
+            }else{
+                System.out.println("You have not entered any arguments so port 12345 and cookie.txt will be used by default");
+                portNumber = "12345";
+                fileName = "cookie.txt";
+            }
 
-        String fileName = args[0];
-        String port = args[1];
-
-
-
-
-        File cookieFile = new File(fileName);
-        if (!cookieFile.exists()){
-            System.out.println("cookie file not found");
-            System.exit(0);
-        }
-
-        // testing the cookie class
+        //test the cookie class
         Cookie cookie = new Cookie();
         cookie.readCookieFile(fileName);
         String myCookie = cookie.getRandomCookie();
         System.out.println(myCookie);
-        String myCookie2 = cookie.getRandomCookie();
-        System.out.println(myCookie2);
+        
 
+        ServerSocket server = new ServerSocket(Integer.parseInt(portNumber));
+        Socket socket = server.accept();
 
-        //slide 8 - establish server connection
-        ServerSocket ss = new ServerSocket(Integer.parseInt(port));
-        Socket socket = ss.accept();
-
-        // store the data sent over from client , e.g. get-cookie
+        //store message sent from client, e.g. get-cookie
         String msgReceived = "";
-
-        // slide 9 - allow server to read and write over the communication channel
+        
         try (InputStream is = socket.getInputStream()){
             BufferedInputStream bis = new BufferedInputStream(is);
             DataInputStream dis = new DataInputStream(bis);
+            String line = dis.readUTF();
 
-            try (OutputStream os = socket.getOutputStream()){
-                BufferedOutputStream bos = new BufferedOutputStream(os);
-                DataOutputStream dos = new DataOutputStream(bos);
+            while (!msgReceived.equals("close")){
+                //instantiate cookie.java
+                //get a random cookie
 
-            //write our logic to receive and send
-            while(!msgReceived.equals("close")){
-                    // slide 9 - receive message
-                    msgReceived = dis.readUTF();
+                String randomCookie = cookie.RandomCookie();
 
-                    if (msgReceived.equals("get-cookie")){
-                        //instantiate cookie.java
-                        // get a random cookie
-                        String randomCookie = cookie.getRandomCookie();
-
-                        //send the random cookie out using DataOutputStream (dos.writeUTF(XXXX)))
-                        dos.writeUTF(randomCookie);
-                        dos.flush();
-                    }
-            }                                                                                                                                                                                                                                                            
-            //closes all output streams in reverse order
-            dos.close();
-            bos.close();
-            os.close();
-            }catch (EOFException ex){
-                ex.printStackTrace();
             }
-            //closes all input stream in reverse order
-            dis.close();
-            bis.close();
-            is.close();
-        }catch (EOFException ex){
-            socket.close();
-            ss.close();
         }
+
+
     }
 }
--- src/main/java/sg/edu/nus/iss/Cookie.java
+++ src/main/java/sg/edu/nus/iss/Cookie.java
@@ -13,42 +13,46 @@ public class Cookie {
     List<String> cookies = null;
 
     public void readCookieFile(String fileName) throws IOException{
-        //instantiate the cookies collection object
+        // instantiate cookie collection object
         cookies = new ArrayList<String>();
-
-        //use a File obejct to pass the fileName
+        
+        //use a File object to pass the fileName
         File cookieFile = new File(fileName);
+
 
         //use FileReader and BufferReader for reading from cookie file
         //instantiate a FileReader follow by a BufferedReader
         FileReader fr = new FileReader(cookieFile);
         BufferedReader br = new BufferedReader(fr);
 
+
         //while loop to loop through the file
         // read each line and add in the cookies collection object
-        String idiom = "";
-        while ((idiom = br.readLine())!=null){
-            cookies.add(idiom);
+        String line = "";
+        while ((line=br.readLine())!=null){
+            cookies.add(line);
         }
-
 
         // close the BufferedReader and FileReader in reverse order
         br.close();
         fr.close();
-
     }
 
+
+    // getRandomCookie
     public String getRandomCookie(){
-        //use Math random function to get random item from cookies collcetion object
+        //use Math random function to get random item from cookies collection
         Random random = new Random();
 
-        //check if cookies collection are loaded
-        // if cookies collection loaded, then generate and return random cookie
-        if (cookies==null){
-            return "There is no cookie.";
+        //check if cookies collections are loaded
+        // if loaded, generate and return random cookie;
+
+        if (cookies ==null){
+            return "There is no cookie";
         }else{
             String randomCookie = cookies.get(random.nextInt(cookies.size()));
             return randomCookie;
         }
     }
+
 }
